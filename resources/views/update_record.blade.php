6f819b21e2a8dc9ded05b3a7965da76eb409e6f9
Jim Neutt23 <f360.jovejera@gmail.com>
2016-02-10 14:49:49 +0800

    partial update forms

--- resources/views/update_record.blade.php
+++ resources/views/update_record.blade.php
@@ -26,32 +26,9 @@
 
 	@if( isset($info) )
 
-		<form id="stage-1" action="/edit" method="post">
-			<input type="text" name="name" placeholder="enter principal name..." value="{{ $info->name }}" />
-			<input type="text" name="claimant" placeholder="enter claimant name..." value="{{ $info->claimant }}" />
-			<input type="text" name="coc" placeholder="enter coc name..." value="{{ $info->coc }}" />
+		@include('inc.stage_1_form')
 
-			<input type="text" name="inception" placeholder="enter inception date..." id="datepicker" value="{{ \Carbon\Carbon::parse($info->inception)->format('m/d/Y') }}">
-			<input type="text" name="dm" placeholder="enter dm name..." value="{{ $info->dm }}" />
-			<input type="text" name="policy" placeholder="enter policy name..." value="{{ $info->policy }}" />
-
-			<label>Documents Status: </label>
-			<select name="docs">
-				<option value="incomplete" {{ ($info->documents == 'incomplete') ? 'selected' : '' }}>Incomplete</option>
-				<option value="complete" {{ ($info->documents == 'complete') ? 'selected' : '' }}>Complete</option>
-			</select>
-
-			<textarea name="docs_comments" placeholder="your documents' comments">{{ $info->documents_comments }}</textarea>
-
-			<span>Encoded: <small>{{ \Carbon\Carbon::parse($info->encoded)->format('m/d/Y') }} </small></span>
-			
-			<input type="text" name="amount" placeholder="enter claim amount"  value="{{ $info->amount }}"/>
-			<span><small>*please double check fields before submitting</small></span><br/>
-			<input type="hidden" name="id" value="{{ $info->id }}">
-			<input type="hidden" name="_token" value="{{ csrf_token() }}">
-			<input type="submit" value="Update" class="button" />
-			<a href="/">cancel</a>
-		</form>
+		<hr>
 	@else
 		<p class="error">
 			If you got here accidentally, please go back <a href="/">home</a>.
