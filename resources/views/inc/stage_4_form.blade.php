30df08fefa0b68fe1faeb6777d99ae3f22b2bb90
Jim Neutt23 <f360.jovejera@gmail.com>
2016-02-10 15:34:38 +0800

    changed stage 4 html

--- resources/views/inc/stage_4_form.blade.php
+++ resources/views/inc/stage_4_form.blade.php
@@ -1,7 +1,7 @@
 		<h3 style="text-decoration: underline;"><strong>Stage 4</strong></h3>
 
 		<form id="stage-3" action="/edit" method="post">
-			<textarea name="followup_comments" placeholder="follow up status here..."></textarea>
+			<label style="cursor: pointer;"><input type="checkbox" name="released" value="yes">Check Released</label><br>
 			<span><small>*please double check fields before submitting</small></span><br/>
 			<input type="hidden" name="id" value="{{ $info->id }}">
 			<input type="hidden" name="_token" value="{{ csrf_token() }}">
