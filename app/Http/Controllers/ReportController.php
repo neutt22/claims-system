d4b97507ac5bc93a15ffc49945e494afa8559433
Jim Neutt23 <f360.jovejera@gmail.com>
2016-02-24 18:44:10 +0800

    Add reporting panel

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Info;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -12,7 +13,82 @@ class ReportController extends Controller
 
     public function getReport()
     {
-        return view('reports');
+        $infos = Info::all();
+
+        $total_claims = count($infos);
+
+        $total_coc = 0;
+        foreach($infos as $info){
+            if($info->coc != null){
+                $total_coc += 1;
+            }
+        }
+
+        $total_dm = 0;
+        foreach($infos as $info){
+            if($info->dm != null){
+                $total_dm += 1;
+            }
+        }
+
+        $total_policy = 0;
+        foreach($infos as $info){
+            if($info->policy != null){
+                $total_policy += 1;
+            }
+        }
+
+        $incomplete_docs = 0;
+        foreach($infos as $info){
+            if($info->documents == 'incomplete'){
+                $incomplete_docs += 1;
+            }
+        }
+
+        $claims_amount = [];
+        $total = 0;
+        $pending = 0;
+        $approved = 0;
+        foreach($infos as $info){
+            $total += $info->amount;
+
+            if($info->claim_status == 'pending'){
+                $pending += $info->amount;
+            }
+
+            if($info->claim_status == 'approved'){
+                $approved += $info->amount;
+            }
+        }
+        $claims_amount['pending'] = $pending;
+        $claims_amount['total'] = $total;
+        $claims_amount['approved'] = $approved;
+
+        $stats = array_fill_keys(array('denied', 'approved', 'closed', 'pending'), 0);
+        foreach($infos as $info){
+            if($info->claim_status == 'denied') $stats['denied'] += 1;
+            if($info->claim_status == 'approved') $stats['approved'] += 1;
+            if($info->claim_status == 'closed') $stats['closed'] += 1;
+            if($info->claim_status == 'pending') $stats['pending'] += 1;
+        }
+
+        $total_check_released = 0;
+        foreach($infos as $info){
+            if($info->check_released == 'yes'){
+                $total_check_released += 1;
+            }
+        }
+
+        return view('reports')
+            ->with('total_claims', $total_claims)
+            ->with('total_coc', $total_coc)
+            ->with('total_dm', $total_dm)
+            ->with('total_policy', $total_policy)
+            ->with('incomplete_docs', $incomplete_docs)
+            ->with('claims_amount', $claims_amount)
+            ->with('stats', $stats)
+            ->with('total_check_released', $total_check_released)
+            ->with('infos', $infos);
     }
 
     public function getQuickReport(){
--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -1,7 +1,83 @@
 @extends('app')
 
 @section('content')
-    Simple report here
+
+    <div class="report-container">
+
+        <a href="/"><img src="{{ asset('img/logo.png') }}" class="logo" ></a>
+
+        <div class="report-head">
+            <h3><strong>GIBX Claims System Reports</strong></h3>
+        </div>
+
+        <div class="report-item">
+            <h2>Total Claims Filed: </h2>
+            <span>{{ $total_claims }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Total COC: </h2>
+            <span>{{ $total_coc }}/{{ $total_claims }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Total DM: </h2>
+            <span>{{ $total_dm }}/{{ $total_claims }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Total Policy: </h2>
+            <span>{{ $total_dm }}/{{ $total_claims }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Incomplete Documents: </h2>
+            <span>{{ $incomplete_docs }}/{{ $total_claims }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Pending Claims Amount: </h2>
+            <span>{{ number_format($claims_amount['pending'], 2, '.', ',') }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Approved Claims Amount: </h2>
+            <span class="good">{{ number_format($claims_amount['approved'], 2, '.', ',') }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Total Claims Amount: </h2>
+            <span>{{ number_format($claims_amount['total'], 2, '.', ',') }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Total Pending Status: </h2>
+            <span>{{ $stats['pending'] }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Total Denied Status: </h2>
+            <span>{{ $stats['denied'] }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Total Closed Status: </h2>
+            <span>{{ $stats['closed'] }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Total Approved Status: </h2>
+            <span class="good">{{ $stats['approved'] }}</span>
+            <hr>
+        </div>
+        <div class="report-item">
+            <h2>Total Check Released: </h2>
+            <span>{{ $total_check_released }}</span>
+            <hr>
+        </div>
+
+    </div>
+
 @endsection
 
 @section('footer')
