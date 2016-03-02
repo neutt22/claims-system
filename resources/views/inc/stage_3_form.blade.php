74add6be4c6233936ab140c65783ff5a39bfbc5b
Jim Neutt23 <f360.jovejera@gmail.com>
2016-03-02 17:08:46 +0800

    Funny bug in Mich's machine

--- resources/views/inc/stage_3_form.blade.php
+++ resources/views/inc/stage_3_form.blade.php
@@ -10,11 +10,12 @@
 			<textarea name="followup_comments" placeholder="follow up status here...">{{ $info->followup_comments }}</textarea>
 			<label style="cursor: pointer;"><input type="checkbox" name="followed_up" value="yes" {{ $info->followed_up == 'yes' ? 'checked' : ''}}>Followed Up</label><br/>
 			<select name="stage_3_status">
-				<option value="pending" {{ ($info->stage_3_status == 'pending') ? 'selected' : '' }}>Pending</option>
-				<option value="approved" {{ ($info->stage_3_status == 'approved') ? 'selected' : '' }}>Approved</option>
-				<option value="denied" {{ ($info->stage_3_status == 'denied') ? 'selected' : '' }}>Denied</option>
-				<option value="closed" {{ ($info->stage_3_status == 'closed') ? 'selected' : '' }}>Closed</option>
+				<option value="pending" @if($info->stage_3_status == 'pending') 'selected' @endif>Pending</option>
+				<option value="approved" @if($info->stage_3_status == 'approved') 'selected' @endif>Approved</option>
+				<option value="denied" @if($info->stage_3_status == 'denied') 'selected' @endif>Denied</option>
+				<option value="closed"  @if($info->stage_3_status == 'closed') 'selected' @endif>Closed</option>
 			</select><br/>
+			{{--<span>{{ $info->stage_3_status }}</span>--}}
 			<label style="font-size: 12px; cursor: pointer; margin-top: 10px; display: inline-block;" for="datepicker2">Date:</label><br/>
 			<input type="text" name="stage_3_date" placeholder="enter a date..." id="datepicker2" value="{{ \Carbon\Carbon::parse($info->stage_3_date)->format('m/d/Y') }}"><br/>
 			{{--<label style="font-size: 12px; cursor: pointer; margin-top: 10px; display: inline-block;" for="datepicker3">MICO Released:</label><br/>--}}
